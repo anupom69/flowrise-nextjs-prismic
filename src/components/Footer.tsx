038f34ceee385763a2817f12464bdf3631dc31e3
Ratna Rani Sarkar <112156364+anupom69@users.noreply.github.com>
2024-09-28 13:18:53 +0600

    feat: Header and Footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,29 @@
 import { createClient } from "@/prismicio";
 import Link from "next/link";
 import { PrismicNextLink } from "@prismicio/next";
+import { FlowriseIcon } from "./Logo";
+import Bounded from "./Bounded";
 
 export default async function Footer() {
   const client = createClient();
   const settings = await client.getSingle("settings");
   return (
-    <footer>
-      <Link href={`/`}>{settings.data.site_title}</Link>
-      <p>©{new Date().getFullYear()} {settings.data.site_title}</p>
-      <ul>
-        {settings.data.navigation.map(({ link, label }) => (
-          <li key={label}>
-            <PrismicNextLink field={link}>{label}</PrismicNextLink>
-          </li>
-        ))}
-      </ul>
-    </footer>
+    <Bounded as="footer">
+      <div className="flex sm:flex-row flex-col justify-between gap-6 items-center">
+        <Link href={`/`}>
+          <FlowriseIcon />
+        </Link>
+        <p className="text-xs">
+          ©{new Date().getFullYear()} {settings.data.site_title}
+        </p>
+        <ul className="flex">
+          {settings.data.navigation.map(({ link, label }) => (
+            <li key={label} className="p-3">
+              <PrismicNextLink field={link}>{label}</PrismicNextLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Bounded>
   );
 }
